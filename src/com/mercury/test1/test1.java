d271440929c3da599fb6dff854203087688e934f
souravpattnaik431 <sauravpattnaik674@gmail.com>
2019-05-26 12:40:12 +0530

    Initial Commit

---
+++ src/com/mercury/test1/test1.java
@@ -0,0 +1,22 @@
+package com.mercury.test1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\saura\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("sauravpattnaik674@gmail.com");
+		driver.findElement(By.name("pass")).sendKeys("123sourav");
+		driver.findElement(By.linkText("Forgotten account?")).click();
+
+	}
+
+}
